2c572fe79af83b5463a07c8b47aef7426ece79d3
Ashis Kumar <ashishsahoo389@gmail.com>
2024-06-08 12:20:53 +0530

    Added NavBar to the Dashboard but Search input need enhancement.

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Image from "next/image";
+import SearchBar from "./SearchBar";
+
+const NavBar = () => {
+  return (
+    <div className="flex gap-8 items-center px-20 py-6">
+      <Image src="/icon.svg" width={194} height={36} alt="jiffyScan" />
+      <p className="text-base font-normal text-gray-500">Home</p>
+      <div className="flex items-center">
+        <p className=" text-base font-normal  text-gray-500">BlockChain</p>
+        <Image src="/arrow.svg" width={24} height={24} alt="arrow" />
+      </div>
+      <div className="flex items-center">
+        <p className=" text-base font-normal  text-gray-500">Developers</p>
+        <Image src="/arrow.svg" width={24} height={24} alt="arrow" />
+      </div>
+      <div className="flex items-center">
+        <p className=" text-base font-normal  text-gray-500">More</p>
+        <Image src="/arrow.svg" width={24} height={24} alt="arrow" />
+      </div>
+      <p className=" text-base font-normal  text-gray-500">About 4337</p>
+      <SearchBar />
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,34 @@
+import React, { ChangeEvent, ClipboardEvent, useState } from "react";
+import Image from "next/image";
+
+const SearchBar = () => {
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const handlePaste = (event: ClipboardEvent) => {
+    const pasteData = event.clipboardData.getData("text");
+    event.preventDefault();
+    setInputValue(pasteData);
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+  return (
+    <div className="flex items-center gap-4 w-[22.5rem] h-10 px-6 py-1 border-2 border-slate-300 rounded-full hover:border-blue-500">
+      <div className="flex items-center gap-1.5">
+        ⚡️
+        <Image src="arrow.svg" width={24} height={24} alt="arrow" />
+      </div>
+      <span className="h-6 border-l border-gray-300 m-3" />
+      <input
+        placeholder="Search an UserOp"
+        className="active:border-transparent"
+        value={inputValue}
+        /* onPaste={handlePaste} */
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
