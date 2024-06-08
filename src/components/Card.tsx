d5b5a8b752c89acba854166034e6f556a5e12ef1
Ashis Kumar <ashishsahoo389@gmail.com>
2024-06-09 00:47:54 +0530

    Added Card component aligned with figma mockup.

---
+++ src/components/Card.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Image from "next/image";
+
+type prop = {
+  path: string;
+  companyName: string;
+  title: string;
+  dateOfEvent?: Date;
+  bountyPrice?: number;
+  position?: string;
+};
+
+const Card = ({
+  path,
+  companyName,
+  title,
+  bountyPrice,
+  position,
+  dateOfEvent,
+}: prop) => {
+  return (
+    <div className="flex w-[67.5rem] h-[7.2rem] p-4 rounded-lg  hover:bg-slate-200">
+      <Image src={path} width={80} height={80} alt={companyName} />
+      <div className="flex w-full p-6 justify-between items-center">
+        <div>
+          <p className="text-xl font-semibold text-neutral-900">
+            {title}&nbsp;<span className="text-sm font-normal">by</span>
+            &nbsp;
+            <span className="text-slate-400">{companyName}</span>
+          </p>
+          <div className="flex items-center">
+            {bountyPrice && position && (
+              <>
+                <Image src="/token.svg" width={24} height={24} alt="token" />
+                <p className="ml-1 text-lg font-semibold">
+                  {bountyPrice}&nbsp;
+                  <span className="text-slate-400">USDC</span>
+                </p>
+                <span className="h-6 border-l border-gray-300 m-3" />
+                <span className="text-gray-500 mr-1">🏆 {position} Place</span>
+              </>
+            )}
+          </div>
+        </div>
+        <div>
+          <p className="text-gray-500">04/04/2022</p>
+          <p className="text-gray-500">32 participants</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
